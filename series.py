1db0eb02c0c1d0005e81f6da73d456c1e8a58163
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-03 18:51:39 -0800

    created fibonacci function and lucas function. tests green

--- series.py
+++ series.py
@@ -1,4 +1,15 @@
+
 def fibonacci(n):
-    return 'hello'
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
 def lucas(n):
-    return 'world'
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-1)+lucas(n-2)
--- test_series.py
+++ test_series.py
@@ -1,11 +1,20 @@
 from series import fibonacci, lucas
 
 def test_one():
-    expected = 'hello'
-    actual = fibonacci('')
+    expected = 3
+    actual = fibonacci(4)
+    assert actual == expected
+def test_two():
+    expected = 13
+    actual = fibonacci(7)
     assert actual == expected
 
-def test_two():
-    expected = 'world'
-    actual = lucas('')
+def test_three():
+    expected = 144
+    actual = fibonacci(12)
+    assert actual == expected
+
+def test_four():
+    expected = 29
+    actual = lucas(7)
     assert actual == expected
