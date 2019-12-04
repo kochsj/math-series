799e75782ff8881ee679e91985b6416c84716660
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-03 19:56:36 -0800

    created sum_series function. wrote tests. red -> green

--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-from series import fibonacci, lucas
+from series import fibonacci, lucas, sum_series
 
 def test_one():
     expected = 3
@@ -17,4 +17,39 @@ def test_three():
 def test_four():
     expected = 29
     actual = lucas(7)
+    assert actual == expected
+
+def test_five():
+    expected = 123
+    actual = lucas(10)
+    assert actual == expected
+
+def test_six():
+    expected = 3
+    actual = sum_series(4)
+    assert actual == expected
+
+def test_seven():
+    expected = 322
+    actual = sum_series(12, 2, 1)
+    assert actual == expected
+
+def test_eight():
+    expected = 75025
+    actual = sum_series(25)
+    assert actual == expected
+
+def test_nine():
+    expected = 178
+    actual = sum_series(8, 4, 6)
+    assert actual == expected
+
+def test_ten():
+    expected = 257
+    actual = sum_series(6, 13, 24)
+    assert actual == expected
+
+def test_eleven():
+    expected = 15737
+    actual = sum_series(17, 3, 8)
     assert actual == expected
